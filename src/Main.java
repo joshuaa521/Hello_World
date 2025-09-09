d362d5bc2edc4175c98a21d76e231fd0fd325d95
JoshuaAddanki <joshuaa521@nycstudents.net>
2025-09-09 15:17:21 -0400

    Added a scanner

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
-public class Main {
-    public static void main (String[] args)
-    {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("What's your name?");
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();
+        System.out.println("Hello, " + name + "!");
     }
 }
